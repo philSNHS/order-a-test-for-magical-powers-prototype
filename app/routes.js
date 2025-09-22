5c92e9a8962e013629bbb6f9a43558548c357d34
Laura Driver <107620097+LauraJD@users.noreply.github.com>
2025-09-22 13:57:06 +0000

    Added ineligible page.

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,31 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Called when answering question about whether NHS number is known
+router.post('/magical-powers-answer', (req, res) => {
+
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const doYouHaveSymptoms = req.session.data.doYouHaveSymptoms
+
+  // Check whether the variable matches a condition
+  if (doYouHaveSymptoms === 'Yes') {
+
+    // Send user to a page where they’ll enter their NHS number
+    res.redirect('/details')
+
+  } else if (doYouHaveSymptoms === 'No') {
+
+    // Send user to a page where they can find their NHS number
+    res.redirect('/ineligible')
+
+  } else {
+
+    // Send user back to the question page
+    res.redirect('/details')
+
+  }
+})
+
+
 
 module.exports = router
